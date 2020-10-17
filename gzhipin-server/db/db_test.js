a761381fcc11cec84648873decd353ba9e86a819
agnes_xxw <agnes_xxw@outlook.com>
2020-10-17 14:58:08 +0800

    feat: crud

--- gzhipin-server/db/db_test.js
+++ gzhipin-server/db/db_test.js
@@ -34,12 +34,42 @@ const UserModel = mongoose.model('user', userSchema)// 集合名称为users
 // 3. 通过 Model 或其实例对集合数据进行 CRUD 操作
 function testSave(){
     const userModel = new UserModel({
-        username: 'Tom',
-        password: md5('1234'),
-        type: 'qiuzhi'
+        username: 'Bob',
+        password: md5('342526'),
+        type: 'laoban'
     })
     userModel.save(function(error, userDoc) {
         console.log('save()', error, userDoc)
     })
 }
-testSave()
+// testSave()
+
+function testFind(){
+    // 返回的是[], 没有匹配的就是一个空数组
+    UserModel.find(function(error, users){
+        console.log('find()', error, users)
+    })
+    // 返回的是文档对象，没有的话是null
+    UserModel.findOne({_id: '5f8a5d924513603c72785f43'}, function(error, user){
+        console.log('findOne()', error, user)
+    })
+}
+
+// testFind()
+function testUpdate(){
+    UserModel.findByIdAndUpdate({
+        _id: '5f8a5d924513603c72785f43'
+    }, {
+        username: 'Sherry'
+    }, function(error, doc){ // 返回的doc是老得对象（更改前的）
+        console.log('findByIdAndUpdate', error, doc)
+    })
+}
+// testUpdate()
+function testDelete(){
+    UserModel.remove({_id:'5f8a5d924513603c72785f43'}, function(error, doc){ // {n: 1/0, ok: amount}
+        console.lod('delete()', error, doc)
+    })
+}
+testRemotestDeleteve()
+
