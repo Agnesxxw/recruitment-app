d9001985506d6242378cf4c1f5bc28adddafa921
agnes_xxw <agnes_xxw@outlook.com>
2020-10-20 21:05:06 +0800

    feat: login/register router and database connected

--- gzhipin-server/db/db_test.js
+++ gzhipin-server/db/db_test.js
@@ -1,15 +1,18 @@
 /*
 使用 mongoose 操作 mongodb 的测试文件 
 1. 连接数据库
-1.1. 引入 mongoose
-1.2. 连接指定数据库(URL 只有数据库是变化的) 1.3. 获取连接对象
-1.4. 绑定连接完成的监听(用来提示连接成功)
+    1.1. 引入 mongoose
+    1.2. 连接指定数据库(URL 只有数据库是变化的) 
+    1.3. 获取连接对象
+    1.4. 绑定连接完成的监听(用来提示连接成功)
 2. 得到对应特定集合的 Model
-2.1. 字义 Schema(描述文档结构)
-2.2. 定义 Model(与集合对应, 可以操作集合)
+    2.1. 字义 Schema(描述文档结构)
+    2.2. 定义 Model(与集合对应, 可以操作集合)
 3. 通过 Model 或其实例对集合数据进行 CRUD 操作
-3.1. 通过 Model 实例的 save()添加数据
-3.2. 通过 Model 的 find()/findOne()查询多个或一个数据 3.3. 通过 Model 的 findByIdAndUpdate()更新某个数据 3.4. 通过 Model 的 remove()删除匹配的数据
+    3.1. 通过 Model 实例的 save()添加数据
+    3.2. 通过 Model 的 find()/findOne()查询多个或一个数据 
+    3.3. 通过 Model 的 findByIdAndUpdate()更新某个数据 
+    3.4. 通过 Model 的 remove()删除匹配的数据
 */
 
 // 1. 连接数据库
@@ -68,7 +71,7 @@ function testUpdate(){
 // testUpdate()
 function testDelete(){
     UserModel.remove({_id:'5f8a5d924513603c72785f43'}, function(error, doc){ // {n: 1/0, ok: amount}
-        console.lod('delete()', error, doc)
+        console.log('delete()', error, doc)
     })
 }
 testRemotestDeleteve()
--- gzhipin-server/routes/index.js
+++ gzhipin-server/routes/index.js
@@ -1,19 +1,48 @@
 var express = require('express');
 var router = express.Router();
 
+const md5 = require('blueimp-md5')
+const filter = {password: 0, __v: 0} // 查询时过滤出指定属性
+const { UserModel } = require('../db/models')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-// 用户注册
+// 注册路由
 router.post('/register', function(req, res){
-  console.log('res')
+  // 读取参数
+  const { username, password, type } = req.body
+  // 处理，先判断用户是否存在（根据username查询），如果存在，返回错误的信息；如果不存在，保存
+  UserModel.findOne({username}, function(err, user) {
+    if (user){
+      res.send({code: 1, msg: '此用户已存在'})
+    } else {
+      new UserModel({username, type, password: md5(password)}).save(function(err, user){
+        // 生成cookie给浏览器保存
+        res.cookie('userid', user._id, {maxAge: 1000 * 60 * 60 * 24 * 7}) // 持久化cookie，一天内都有效果
+        // 返回响应数据(不携带密码)
+        const data = {username, type, _id: user._id} 
+        res.send({code: 0, data})
+      })
+    }
+  })
+
+  
+})
+
+// 登陆路由
+router.post('/login', function(req, res){
+  // 读取参数
   const { username, password } = req.body
-  if(username === 'admin'){
-    res.send({code: 1, msg: '此用户已存在'})
-  } else {
-    res.send({code: 0, data: {id:'abc123', username, password}})
-  }
+  UserModel.findOne({username, password: md5(password)}, filter, function(err, user){
+    if(user){
+      res.cookie('userid', user._id, {maxAge: 1000 * 60 * 60 * 24 * 7})
+      res.send({code: 0, data: user})
+    }else{
+      res.send({code: 1, msg: '用户名或密码不正确'})
+    }
+  })
 })
 module.exports = router;
