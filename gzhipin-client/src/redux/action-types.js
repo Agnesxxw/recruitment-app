0fe6c4db5727e3493c5f454f4184cffa170a3d6f
agnes_xxw <agnes_xxw@outlook.com>
2020-11-04 00:12:07 +0800

    feat: complete action

--- gzhipin-client/src/redux/action-types.js
+++ gzhipin-client/src/redux/action-types.js
@@ -2,6 +2,6 @@
  * 包含n个action type名称常量
  */
 
- export const SUCCESS_MSG = "AUTH_MSG";
- export const ERROR_MSG = "ERROR_MSG"
+ export const AUTH_SUCCESS = "AUTH_MSG"; // 登陆成功
+ export const ERROR_MSG = "ERROR_MSG"// 请求前： 是否非法；请求后： 返回code是1
  
--- gzhipin-client/src/redux/actions.js
+++ gzhipin-client/src/redux/actions.js
@@ -4,7 +4,18 @@
  * 同步action
  */
 import {reqRegister, reqLogin} from '../api'
- 
+ import { AUTH_SUCCESS, ERROR_MSG } from './action-types'
+ // 授权成功的action
+const authSuccess = (user) => ({
+    type: AUTH_SUCCESS,
+    data: user
+})
+// 错误提示信息的action
+const errorMsg = (msg) => ({
+    type: ERROR_MSG,
+    data: msg
+})
+
 // 注册异步action
 export const register = (user) => {
     // 发送注册请求
@@ -25,8 +36,11 @@ export const login = (user) => {
         const response = await reqLogin(user)
         const result = response.data
         if(result.code === 0){ // 成功
-
+            // 分发成功的action
+            dispatch(authSuccess(result.data))
         } else { // 失败
+            // 分发失败的action
+            dispatch(errorMsg(result.msg))
         }
     }
 }
